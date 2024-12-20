9efd684bd6ce7b6b14e2e0c8eac43f54381fc7e3
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-12-20 21:34:01 +0000

    fix 'Abort Error' display on UI

--- src/App.js
+++ src/App.js
@@ -1,53 +1,6 @@
 import { useEffect, useState } from 'react';
 import StartRating from './StarRating';
 
-const tempMovieData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: 'tt0088763',
-    Title: 'Back to the Future',
-    Year: '1985',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -80,7 +33,7 @@ export default function App() {
           setMovies(data.Search);
           setLoadingError('');
         } catch (error) {
-          if (error.message !== 'AboutError') {
+          if (error.name !== 'AbortError') {
             setLoadingError(error.message);
           }
         } finally {
@@ -111,8 +64,6 @@ export default function App() {
     setSelectedMovieID((selectedMovieID) =>
       selectedMovieID === movieID ? handleCloseMovie() : movieID,
     );
-
-    console.log('movieID: ', movieID);
   }
 
   function handleAddWatched(movie) {
@@ -311,11 +262,16 @@ function MovieDetails({ movieID, onCloseMobie, onAddWatched, watched }) {
 
   useEffect(
     function () {
-      document.addEventListener('keydown', function (e) {
+      function callBack(e) {
         if (e.code === 'Escape') {
           onCloseMobie();
         }
-      });
+      }
+      document.addEventListener('keydown', callBack);
+
+      return function () {
+        document.removeEventListener('keydown', callBack);
+      };
     },
     [onCloseMobie],
   );
--- src/index.js
+++ src/index.js
@@ -2,35 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import StartRating from './StarRating';
-import { useState } from 'react';
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  function handleSetMovieRating(ratings) {
-    setMovieRating(ratings);
-  }
-
-  return (
-    <div>
-      <StartRating onSetRating={handleSetMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StartRating maxRating={4} color='red' size='24' /> */}
-    {/* <Test /> */}
-    {/* <StartRating
-      maxRating={4}
-      color='blue'
-      size='32'
-      messages={['Very good', 'Good', 'Bad', 'Very bad']}
-    /> */}
   </React.StrictMode>,
 );
