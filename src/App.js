e6c19a2c88aefd64ac248fd6cc2ac586d76f570e
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-12-21 22:38:36 +0000

    use useRef to focus on search input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import StartRating from './StarRating';
 
 const average = (arr) =>
@@ -146,6 +146,12 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
+
   return (
     <input
       className='search'
@@ -153,6 +159,7 @@ function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
